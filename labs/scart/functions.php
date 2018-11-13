4c68cf2f08b38dbca6140217aa220c855a5b9f7f
Beenish Syed <bsyed@csumb.edu>
2018-11-13 21:07:41 +0000

    Adding lab4 sopping cart

---
+++ labs/scart/functions.php
@@ -0,0 +1,91 @@
+<?php
+function displayResults()
+{
+    global $items;
+    if(isset($items))
+    {
+        /*
+        //Checing how the array is storing stuff
+        for($i=0; $i<3;$i++)
+        {
+            echo $items[$i]['name'];
+            echo "<br>";
+        }*/
+        echo "<table class='table'>";
+        foreach ($items as $item)
+        {
+            $itemName=$item['name'];
+            $itemPrice=$item['salePrice'];
+            $itemImage=$item['thumbnailImage'];
+            $itemId=$item['itemId'];
+            
+            //display items as table row
+            echo "<tr>";
+            echo "<td> <img src='$itemImage'></td>";
+            echo "<td><h4>$itemName</h4></td>";
+            echo "<td><h4>$itemPrice</h4></td>";
+            //hidden input element containing the item name
+            echo "<form method='post'>";
+            echo "<input type='hidden' name='itemName' value='$itemName'>";
+            echo "<input type='hidden' name='itemId' value='$itemId'>";
+            echo "<input type='hidden' name='itemImage' value='$itemImage'>";
+            echo "<input type='hidden' name='itemPrice' value='$itemPrice'>";
+            if($_POST['itemId']==$itemId)
+            {
+                echo "<td><button class='btn btn-warning'>Added</button></td>";
+            }
+            else
+            {
+                echo "<td><button class='btn btn-warning'>Add</button></td>";
+            }
+            echo "</form>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }
+     
+}
+
+function displayCart()
+{
+    if(isset($_SESSION['cart']))
+    {
+        echo "this is the cart";
+        echo "<table class='table'>";
+        foreach ($_SESSION['cart'] as $item)
+        {
+            $itemName=$item['name'];
+            $itemPrice = $item['price'];
+            $itemImage = $item['image'];
+            $itemId=$item['id'];
+            $itemQuant= $item['quantity'];
+            
+            //display item as table row
+            echo "<tr>";
+            echo "<td><img src = '$itemImage'></td>";
+            echo "<td><h4>$itemName</h4></td>";
+            echo "<td><h4>$itemPrice</h4></td>";
+            //echo "<td><h4>$itemQuant</h4></td>";
+            //update form for this item
+            echo '<form method ="post">';
+            echo "<input type='hidden' name='itemId' value='$itemId'>";
+            echo "<td><input type='text' name='update' class='form-control' placeHolder='$itemQuant'></td>";
+            echo "<td><button class='btn btn-warning'>Update</button></td>";
+            echo "</form>";
+            
+            //hidden input element containing the item name
+            echo "<form method='post'>";
+            echo "<input type ='hidden' name='removeID' value ='$itemId'>";
+            echo "<td><button class='btn btn-warning'>Add</button></td>";
+            echo "</form>";
+            echo "</tr>";
+        }
+        echo "</table>";
+    }   
+}
+
+function displayCartCount()
+{
+    echo count($_SESSION['cart']);
+}
+?>
